ed1eeb98582611f6f7bc8ce2a709225ff905b65a
dietzdenstore <jonasmogelgaard@gmail.com>
2023-10-27 13:36:00 +0200

    Fixed int Balance.

--- src/game/World.java
+++ src/game/World.java
@@ -10,14 +10,17 @@ class World {
         Space capital = new SpaceCity("Capital");
         Space city = new SpaceCity("City");
         Space town = new SpaceCity("Town");
+        Space village = new SpaceCity("Village");
 
         map.addEdge("Capital", capital);
         map.addEdge("City", city);
         map.addEdge("Town", town);
+        map.addEdge("Village",village);
 
         capital.addEdge("Map", map);
         city.addEdge("Map", map);
         town.addEdge("Map", map);
+        village.addEdge("Map",map);
 
         this.entry = map;
     }
