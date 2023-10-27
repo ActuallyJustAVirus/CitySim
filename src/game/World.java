0d6a9e83bcc475c373ea028d58de4de9fd111c6f
mohamed <you@example.com>
2023-10-27 13:46:05 +0200

    added cities

--- src/game/World.java
+++ src/game/World.java
@@ -11,16 +11,22 @@ class World {
         Space city = new SpaceCity("City");
         Space town = new SpaceCity("Town");
         Space village = new SpaceCity("Village");
+        Space Hamlet= new SpaceCity("Hamlet");
+        Space Locality=new Space("Locality");
 
         map.addEdge("Capital", capital);
         map.addEdge("City", city);
         map.addEdge("Town", town);
         map.addEdge("Village",village);
+        map.addEdge("Hamlet", Hamlet);
+        map.addEdge("Locality", Locality);
 
         capital.addEdge("Map", map);
         city.addEdge("Map", map);
         town.addEdge("Map", map);
         village.addEdge("Map",map);
+        Locality.addEdge("map", map);
+        Hamlet.addEdge("map", map);
 
         this.entry = map;
     }
