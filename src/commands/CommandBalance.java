88d4e363c87678f34e30046265b4d25e8991dfe5
Mikkel Nielsen <mikkel1510@live.dk>
2023-10-27 13:00:33 +0200

    added }

--- src/commands/CommandBalance.java
+++ src/commands/CommandBalance.java
@@ -7,4 +7,5 @@ public class CommandBalance extends BaseCommand {
     public void execute(Context context, String command, String[] parameters) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    }
 }
