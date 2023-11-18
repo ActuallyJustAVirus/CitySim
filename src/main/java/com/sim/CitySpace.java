c04308b1aa13c071a86cab635a84fd6b87b51df8
dietzdenstore <jonasmogelgaard@gmail.com>
2023-11-18 17:49:10 +0100

    Added booleans for institutions in CitySpace.java, and added an example in Capital in world.java

--- src/main/java/com/sim/CitySpace.java
+++ src/main/java/com/sim/CitySpace.java
@@ -1,14 +1,23 @@
 package com.sim;
+import java.util.*;
 
 public class CitySpace extends Space {
     int x, y;
+    List<Road> roads;
+    boolean hasHospital, hasPoliceStation, hasFireStation, hasSchool, hasWorkplace;
 
-    CitySpace(String name, int x, int y) {
+    CitySpace(String name, int x, int y, boolean hasHospital, boolean hasPoliceStation, boolean hasFireStation, boolean hasSchool, boolean hasWorkplace) {
         super(name);
         this.x = x;
         this.y = y;
+        this.hasHospital = hasHospital;
+        this.hasPoliceStation = hasPoliceStation;
+        this.hasFireStation = hasFireStation;
+        this.hasSchool = hasSchool;
+        this.hasWorkplace = hasWorkplace;
     }
 
+    // Added booleans for institutions. TODO: Figure out what cities has access to what institutions.
     @Override
     public void welcome() {
         System.out.println("You are in " + name);
