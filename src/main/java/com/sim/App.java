e166e7de70eadc481269c33e7fb463f2d0b35b45
JustAVirus <bern64390@gmail.com>
2023-11-18 15:56:33 +0100

    more panes

--- src/main/java/com/sim/App.java
+++ src/main/java/com/sim/App.java
@@ -8,12 +8,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
     
     private static Scene scene;
     private static Canvas canvas;
+    private static AnchorPane overlay;
 
     @Override
     public void start(Stage stage) throws IOException {
@@ -21,14 +23,16 @@ public class App extends Application {
         stage.setScene(scene);
         stage.show();
         canvas = (Canvas) scene.lookup("#canvas");
+        overlay = (AnchorPane) scene.lookup("#overlay");
         GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.setFill(javafx.scene.paint.Color.RED);
         gc.fillRect(0, 0, 10000, 10000);
         gc.fillText("Hello", 10, 10);
-        canvas.setOnMouseClicked(e -> {
-            gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        overlay.setOnMouseClicked(e -> {
+            // gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+            gc.setFill(javafx.scene.paint.Color.BLACK);
             gc.fillText("Hello", e.getX(), e.getY());
         });
-
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
--- src/main/java/com/sim/Controller.java
+++ src/main/java/com/sim/Controller.java
@@ -5,4 +5,8 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 
 public class Controller {
+    @FXML
+    private void handleButtonAction() throws IOException {
+        System.out.println("You clicked me!");
+    }
 }
