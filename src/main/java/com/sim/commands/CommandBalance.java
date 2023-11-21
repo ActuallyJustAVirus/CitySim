69edea4e9db7d7e9b491d314514ea172d775c13a
dietzdenstore <jonasmogelgaard@gmail.com>
2023-11-21 09:48:55 +0100

    description added to CommandBalance

--- src/main/java/com/sim/commands/CommandBalance.java
+++ src/main/java/com/sim/commands/CommandBalance.java
@@ -3,6 +3,9 @@ package com.sim.commands;
 import com.sim.Context;
 
 public class CommandBalance extends BaseCommand {
+    public CommandBalance() {
+        description = "Check your financial status";
+    }
     @Override
     public void execute(Context context, String command, String[] parameters) {
     // metode der fremvise din nuværende kampital
